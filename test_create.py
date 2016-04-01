d9ef75fa76a6bff453d5a0a034f15d41ae962a1d
Kali Kaneko (leap communications) <kali@leap.se>
2016-04-01 19:44:07 -0400

    fix creation test, try hand at paisley

--- test_create.py
+++ test_create.py
@@ -2,6 +2,7 @@ import sys
 
 import paisley
 
+from twisted.web import http
 from twisted.internet import reactor, defer
 from twisted.python import log
 
@@ -10,20 +11,21 @@ def test():
     print "STARTING TEST"
     foo = paisley.CouchDB('localhost')
     print "\nCreate database 'testdb':"
-    d = foo.createDB('testdb')
+    d = foo.createDB('testdb1')
     d.addCallback(lambda f: log.err(f))
     wfd = defer.waitForDeferred(d)
     yield wfd
     try:
         print wfd.getResult()
-    except error.Error, e:
+    except Exception as e:
         # FIXME: not sure why Error.status is a str compared to http constants
-        if e.status == str(http.UNAUTHORIZED):
+        if hasattr(e, 'status') and e.status == str(http.UNAUTHORIZED):
             print "\nError: not allowed to create databases"
             reactor.stop()
             return
         else:
             raise
+
     print "\nList databases on server:"
     d = foo.listDB()
     wfd = defer.waitForDeferred(d)
