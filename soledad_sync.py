0e917a1c538d2a00e07e860edb91b6b98c91ae7d
Kali Kaneko (leap communications) <kali@leap.se>
2016-05-23 20:43:00 -0400

    pep8

--- soledad_sync.py
+++ soledad_sync.py
@@ -2,11 +2,12 @@ import os
 from twisted.internet import reactor
 from leap.soledad.client import Soledad
 
+
 def _get_soledad_instance(uuid, passphrase, basedir, server_url, cert_file,
                           token):
     # setup soledad info
-    #logger.info('UUID is %s' % uuid)
-    #logger.info('Server URL is %s' % server_url)
+    # logger.info('UUID is %s' % uuid)
+    # logger.info('Server URL is %s' % server_url)
     secrets_path = os.path.join(
         basedir, '%s.secret' % uuid)
     local_db_path = os.path.join(
@@ -29,8 +30,8 @@ def notify_when_ready(result):
     reactor.stop()
 
 sol = _get_soledad_instance(
-        'defd3db2cc8a749d12a9b56a638b9993', 'pass', '/tmp/',
-        'http://localhost:2323', '', '')
+    'defd3db2cc8a749d12a9b56a638b9993', 'pass', '/tmp/',
+    'http://localhost:2323', '', '')
 sol.create_doc({'test': 42})
 
 d = sol.sync()
